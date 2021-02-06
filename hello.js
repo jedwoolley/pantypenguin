94748a28506f044a41244b48af53c503758eede3
Jed <262685+jedwoolley@users.noreply.github.com>
2021-02-06 15:15:14 -0700

    starting over

--- hello.js
+++ hello.js
@@ -1,45 +1,12 @@
 const http = require('http');
+
 const hostname = 'localhost';
 const port = 3000;
 
-const helpText = `
-<!DOCTYPE html>
-<html>
-<head>
-<title>Your NodeJS Droplet</title>
-<style>
-    body {
-        width: 55em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-        background: #AAAAAA;
-    }
-    div {
-      padding: 30px;
-      background: #FFFFFF;
-      margin: 30px;
-      border-radius: 5px;
-      border: 1px solid #888888;
-    }
-    code {
-      font-size: 16px;
-      background: #DDDDDD;
-    }
-</style>
-</head>
-<body>
-  <div>
-    <h1>We did it!</h1>
-    <p>next step... SSL cert
-  </div>
-</body>
-</html>
-`
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(helpText);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
 });
 
 server.listen(port, hostname, () => {
